6d9bcbad01963e3ef09382630543c02292d00b42
RayWLMo <Raymond.WL.Mo@gmail.com>
2021-06-23 12:28:06 +0100

    changed input text on task.py

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ print("Hello " + name.capitalize().strip() + "!")
 # Version 2
 
 full_name = "full name"
-full_name = input("Please enter your full name using a comma to separate your first and last name:  ")
+full_name = input("Please enter your full name:  ")
 first_name = (full_name.split())[0]
 last_name = (full_name.split())[1]
 print(f"Hi {name.capitalize().strip()}! Your full name is {first_name.capitalize().strip()} {last_name.capitalize().strip()}. Welcome to PyCharm!")
